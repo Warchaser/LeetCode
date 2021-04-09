5b320ded82b7833ebb52cf1b80cd0caa9be8629b
achan <wu.shb@reachauto.com>
2021-04-09 09:29:35 +0800

    fix # Add DeleteDuplicates. Signed-off-by: achan <wu.shb@reachauto.com>
    Change-Id: I3a0af5494f88b4d0609440621cd4c538acd2b953

--- src/DeleteDuplicates.java
+++ src/DeleteDuplicates.java
@@ -1,2 +1,34 @@
+import Bean.ListNode;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class DeleteDuplicates {
+
+    public static ListNode deleteDuplicates(ListNode head){
+        if(head == null){
+            return head;
+        }
+
+        ListNode cur = head;
+
+        while (cur.next != null){
+            if(cur.val == cur.next.val){
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        final ListNode head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+
+        deleteDuplicates(head);
+
+        System.out.println(Arrays.toString(Util.printListFromTailToHead(head).toArray()));
+    }
+
 }
--- src/Util.java
+++ src/Util.java
@@ -1,13 +1,34 @@
 import Bean.ListNode;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Util {
 
     static void logList(ListNode head){
         ListNode node = head;
         while (node.next != null){
-            System.out.print(node.val + " ");
+            System.out.println(node.val + " ");
             node = node.next;
         }
+    }
+
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(listNode == null) {
+            return res;
+        }
+        Stack<ListNode> nodes = new Stack<ListNode>();
+        ListNode head = listNode;
+        while(head != null) {
+            nodes.push(head);
+            head = head.next;
+        }
+        while(!nodes.empty()) {
+            res.add(nodes.peek().val);
+            nodes.pop();
+        }
+        return res;
     }
 
 }
