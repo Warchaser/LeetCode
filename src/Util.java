b3e0b122f31d5909e260bf794cee27ae6f287c84
wu.shb <wu.shb@reachauto.com>
2022-06-27 08:38:11 +0800

    12. 整数转罗马数字 Signed-off-by: wu.shb <wu.shb@reachauto.com>
    Change-Id: I77996242127c20777729e92d7e45085e1f461810
    
    # Conflicts:
    #	src/Util.java

--- src/Util.java
+++ src/Util.java
@@ -47,25 +47,25 @@ public class Util {
         return head;
     }
 
-    public static TreeNode arrayToBTree(Integer[] array){
-        if(array.length == 0){
+    public static TreeNode arrayToBTree(Integer[] array) {
+        if (array.length == 0) {
             return null;
         }
         TreeNode root = new TreeNode(array[0]);
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         boolean isLeft = true;
-        for(int i = 1; i < array.length; i++){
+        for (int i = 1; i < array.length; i++) {
             TreeNode node = queue.peek();
             Integer item = array[i];
-            if(isLeft){
-                if(item != null){
+            if (isLeft) {
+                if (item != null) {
                     node.left = new TreeNode(item);
                     queue.offer(node.left);
                 }
                 isLeft = false;
             } else {
-                if(item != null){
+                if (item != null) {
                     node.right = new TreeNode(item);
                     queue.offer(node.right);
                 }
@@ -74,6 +74,10 @@ public class Util {
             }
         }
         return root;
+    }
+
+    public static void currentMillis(){
+        System.out.println(System.currentTimeMillis());
     }
 
 }
